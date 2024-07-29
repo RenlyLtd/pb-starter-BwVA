347c23479e5bf118e7e4a5d801e4b2d25642bab5
Zishan Ahmed <ziahm8120@gmail.com>
2024-07-29 14:21:43 +0100

    test creating a product variant using hooks

--- pb_hooks/test.pb.js
+++ pb_hooks/test.pb.js
@@ -1,9 +1,32 @@
 onAfterBootstrap((e) => {
-  console.log(JSON.stringify(e.app));
+  console.log("Your pocketbase instance is up and running");
 });
 
-onRecordAfterCreateRequest((e) => {
-  console.log(JSON.stringify(e.httpContext));
-  console.log(JSON.stringify(e.record));
-  console.log(JSON.stringify(e.uploadedFiles));
-});
+onRecordAfterCreateRequest(function (e) {
+  var facetValues = e.record.expandAll("facet_values");
+
+  if (facetValues != null) {
+    // Iterate over each facet value and create a new record in the product_variants collection
+    for (var i = 0; i < array.length; i++) {
+      var id = array[i];
+      try {
+        var collection = $app
+          .dao()
+          .findCollectionByNameOrId("product_variants");
+
+        var record = new Record(collection, {
+          product: e.record.id,
+          stock_count: 1,
+          facet_value: id,
+        });
+
+        $app.dao().saveRecord(record);
+      } catch (error) {
+        console.error(
+          "Error creating product_variant for facet value " + id + ":",
+          error
+        );
+      }
+    }
+  }
+}, "products");
