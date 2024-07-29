0e8d8351493e719efb61a46b5a628b57751e1c65
Zishan Ahmed <ziahm8120@gmail.com>
2024-07-29 13:56:25 +0100

    added console log on record create

--- pb_hooks/test.pb.js
+++ pb_hooks/test.pb.js
@@ -1,3 +1,9 @@
 onAfterBootstrap((e) => {
   console.log(e.app);
 });
+
+onRecordAfterCreateRequest((e) => {
+  console.log(e.httpContext);
+  console.log(e.record);
+  console.log(e.uploadedFiles);
+});
